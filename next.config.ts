0a2139ed2f4833a3d3811ba10618f50e968041c4
kogutu <jacek.skorzynski@gmail.com>
2026-03-20 15:37:32 +0100

    update lockfile with new dependencies

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,26 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['wsrv.nl', "sklep.carinii.com.pl", "carinii.com.pl", 'images.weserv.nl'],
+    // USUŃ TĘ LINIĘ - jest deprecated:
+    // domains: ['wsrv.nl', "sklep.carinii.com.pl", "carinii.com.pl", 'images.weserv.nl'],
+
     formats: ['image/webp', 'image/avif'],
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'wsrv.nl',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sklep.carinii.com.pl',
+      },
+      {
+        protocol: 'https',
         hostname: 'carinii.com.pl',
-        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.weserv.nl',
       },
     ],
   },
