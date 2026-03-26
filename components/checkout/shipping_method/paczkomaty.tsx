91e07b1bc82be1a82bc715e2175b9b922850ce57
kogutu <jacek.skorzynski@gmail.com>
2026-03-26 20:08:49 +0100

    after

--- components/checkout/shipping_method/paczkomaty.tsx
+++ components/checkout/shipping_method/paczkomaty.tsx
@@ -22,7 +22,7 @@ export default function Paczkomaty() {
     const [open, setOpen] = useState(false)
     const isDesktop = useMediaQuery('(min-width: 768px)')
 
-    const token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJzQlpXVzFNZzVlQnpDYU1XU3JvTlBjRWFveFpXcW9Ua2FuZVB3X291LWxvIn0.eyJleHAiOjIwODkxODU3NTEsImlhdCI6MTc3MzgyNTc1MSwianRpIjoiYjM5ZDA2OTctOTllYi00NWUyLTlmNjQtNThlNWQ2OGNlMWU5IiwiaXNzIjoiaHR0cHM6Ly9sb2dpbi5pbnBvc3QucGwvYXV0aC9yZWFsbXMvZXh0ZXJuYWwiLCJzdWIiOiJmOjEyNDc1MDUxLTFjMDMtNGU1OS1iYTBjLTJiNDU2OTVlZjUzNTpjY2VlVUI5QXFvOXdsR3hZcjdGQ2N6bHQ4Q2pQeGhPcmVOX01GS055ODZNIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoic2hpcHgiLCJzZXNzaW9uX3N0YXRlIjoiZjk2OTE4OGMtMWQzMy00ZTExLWE3ZWQtZDU2ZjI4YWYwNzQyIiwic2NvcGUiOiJvcGVuaWQgYXBpOmFwaXBvaW50cyIsInNpZCI6ImY5NjkxODhjLTFkMzMtNGUxMS1hN2VkLWQ1NmYyOGFmMDc0MiIsImFsbG93ZWRfcmVmZXJyZXJzIjoiMTkyLjE2OC4xLjI5IiwidXVpZCI6Ijg1NGFkZmZjLWQyYWEtNGVmZC1iZTA2LTk2MzJjYzkyNGQyYyJ9.SDk6iIZxbWR17rsDWrGcYLMKYhM1hUDLXNzi9OcvxLESlw5kZ1VHHOAc4XKcmVq7lBBc9YeEVgaLmm9nApgQOkYoFSxwIFCDh7E5_OcJ7SJIltA1qBMVWi6t7oDN9odBkBjUmNERQ1Tgt2gHDzenYhovI-A5JrJ7GT-mdCfwTZM_1or-ACtfsGiEfvbAiyNnFvErgpYDVeUCl_IQYssg3-2VvX2sdYqymaKOKA7oszO8h0-QQAaa7SVFoQirCpvu7HVbcBMR3qRzw-fZX8gwSDtHavpyzckGaIMI2RcfFpomO99sD8afIOCzX7VlULj0e43xNbrQTn7wC6lgiWVy5g'
+    const token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJzQlpXVzFNZzVlQnpDYU1XU3JvTlBjRWFveFpXcW9Ua2FuZVB3X291LWxvIn0.eyJleHAiOjIwODk5MTIxMDAsImlhdCI6MTc3NDU1MjEwMCwianRpIjoiZTBmODBhYzAtYmMxZC00NTViLWI1ZDAtMGFjNTYxODg5Yjk5IiwiaXNzIjoiaHR0cHM6Ly9sb2dpbi5pbnBvc3QucGwvYXV0aC9yZWFsbXMvZXh0ZXJuYWwiLCJzdWIiOiJmOjEyNDc1MDUxLTFjMDMtNGU1OS1iYTBjLTJiNDU2OTVlZjUzNTpjY2VlVUI5QXFvOXdsR3hZcjdGQ2N6bHQ4Q2pQeGhPcmVOX01GS055ODZNIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoic2hpcHgiLCJzZXNzaW9uX3N0YXRlIjoiNjEwOWNlMDItMmQwNi00MjQ3LTg2ZjUtMzQ0NjhjNGIyYWU5Iiwic2NvcGUiOiJvcGVuaWQgYXBpOmFwaXBvaW50cyIsInNpZCI6IjYxMDljZTAyLTJkMDYtNDI0Ny04NmY1LTM0NDY4YzRiMmFlOSIsImFsbG93ZWRfcmVmZXJyZXJzIjoiMTY4LjExOS44LjI1NCIsInV1aWQiOiI4NTRhZGZmYy1kMmFhLTRlZmQtYmUwNi05NjMyY2M5MjRkMmMifQ.MDAOXqxVLJyhlOCScXbTKD98lCqdjZPbjiWU94TR0hNTC0iYi2d_QdHSGjBd8vu02xAkFx_IA23sq-ZZ9Q0rykRn9E4lTxRjnLP5yZ2XdiyM2ja6AE-QAWyDW_hsXt9YBkaqiL_KspyFXx_MhAslQvtju14iwULT9jhj46-qoSZsosmJiHDNNFtPUhdCPm2SKLXrx2Lx5cHgG3gvrcFUBKk0Runj-PvuWwolpmSnOuhAS_9Le9JS9jANe7FIispK7Xb4knj8mEHZdmsB42VRIYvwetxgrsKtbNbeLuobKnqaRlG33IN8klou7JR7Hg9YchLepbPvw9Eq2zsxV4i1og'
     const identifier = 'Geo1'
     const language = 'pl'
     const config = 'parcelcollect'
@@ -53,7 +53,7 @@ export default function Paczkomaty() {
             <Dialog open={open} onOpenChange={setOpen}>
                 <DialogTrigger asChild>
                     <Button className='text-xs bg-black/70 '>
-                        Wybierz paczkomat+++
+                        Wybierz paczkomat
                     </Button>
                 </DialogTrigger>
                 <DialogContent className="sm:max-w-[800px]">
@@ -70,14 +70,14 @@ export default function Paczkomaty() {
         <Drawer open={open} onOpenChange={setOpen}>
             <DrawerTrigger asChild>
                 <Button variant="outline">
-                    Wybierz paczkomat+++
+                    Wybierz paczkomat
                 </Button>
             </DrawerTrigger>
             <DrawerContent>
                 <DrawerHeader>
                     <DrawerTitle>Wybierz paczkomat</DrawerTitle>
                 </DrawerHeader>
-                <div className="px-4 pb-4">+++
+                <div className="px-4 pb-4">
                     <PaczkomatyContent />
                 </div>
             </DrawerContent>
